acbcf743473ebd8d15eda1c5fa88b9b542aa8605
Edward Wibowo <wibow9770@gmail.com>
2024-02-03 15:13:44 -0500

    feat: use otamatone model

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
 import { useEffect, useState } from "react";
 import AudioContext, { autoCorrelate } from "./contexts/AudioContext";
-import { Canvas, useFrame } from "@react-three/fiber";
-import {useGLTF, Environment } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, Environment, Stars, Sparkles } from "@react-three/drei";
+
+const LOW_FREQUENCY = 100;
+const HIGH_FREQUENCY = 500;
+const INTERPOLATION_FACTOR = 0.07;
 
 const audioContext = AudioContext.getAudioContext();
 const analyser = audioContext.createAnalyser();
 
 var array = new Float32Array(2048);
 
-function Player({x}) {
-  return (
-    <mesh position={[x, 0, -150]}>
-      <sphereGeometry args={[5, 64, 64]}/>
-      <meshStandardMaterial color="red" />
-    </mesh>
-  );
+function Player({ position, setPosition, target }) {
+  const { scene } = useGLTF("/otamatone.glb");
+  scene.traverse((child) => {
+    if (child.isMesh) {
+      child.material.color.set("cyan");
+    }
+  });
+  setPosition(position + (target - position) * INTERPOLATION_FACTOR);
+
+  return <primitive position={[-400, position, -600]} object={scene} />;
 }
 
 function App() {
   const [source, setSource] = useState(null);
   const [frequency, setFrequency] = useState(0);
+  const [playerPosition, setPlayerPosition] = useState(0);
 
   useEffect(() => {
     if (source) {
@@ -32,28 +40,28 @@ function App() {
 
     const ac = autoCorrelate(array, audioContext.sampleRate);
     if (ac > -1) {
-      setFrequency(parseFloat(ac.toFixed(2)));
+      const frequency = parseFloat(ac);
+      if (frequency >= LOW_FREQUENCY && frequency <= HIGH_FREQUENCY) {
+        setFrequency(frequency);
+      }
     }
   };
 
-  setInterval(pollPitch, 1);
-  console.log(frequency);
+  setInterval(pollPitch, 1000);
 
   useEffect(() => {
     const start = async () => {
       const input = await getMicInput();
-  
+
       if (audioContext.state === "suspended") {
         audioContext.resume();
       }
-  
+
       const source = audioContext.createMediaStreamSource(input);
       setSource(source);
     };
     start();
   }, []);
-
-  
 
   const getMicInput = () => {
     return navigator.mediaDevices.getUserMedia({
@@ -66,15 +74,19 @@ function App() {
     });
   };
 
-
-
   return (
     <>
-      <Canvas shadows camera={{ fov: 60, position: [0, 0, 0]}}>
+      <Canvas shadows camera={{ fov: 60, position: [0, 0, 0] }}>
         <color attach="background" args={["#000000"]} />
         <ambientLight intensity={0.1} />
         <Environment preset="warehouse" />
-        <Player x={frequency / 10}/>
+        <Player
+          position={playerPosition}
+          setPosition={setPlayerPosition}
+          target={(-frequency + 200) * 1.5}
+        />
+        <Stars />
+        <Sparkles />
       </Canvas>
     </>
   );
